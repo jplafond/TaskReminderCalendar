c85b9b54b525345bfa1e2488a03603b3f719a89e
Jp <jp.lafond@gmail.com>
2018-09-02 12:54:20 -0700

    Initial Commit

---
+++ TaskReminderCalendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskReminderCalendar
+//
+//  Created by Jp LaFond on 9/2/18.
+//  Copyright © 2018 Jp LaFond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
